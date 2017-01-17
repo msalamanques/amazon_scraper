a4a57266dce5c2e586fe54f2abdf2ea90a33f4b4
Miguel Salamanques <miguel@fagoramerica.com>
2017-01-17 17:21:13 -0500

    io logic added to test restclient

--- test_restclient.rb
+++ test_restclient.rb
@@ -21,7 +21,7 @@ options = {
 }
 
 fileName = "#{date}_#{time}_#{urlList}.json"
-
+=begin
 # run the crawl
 RestClient.put "https://#{userToken}:@api.80legs.com/v2/crawls/#{crawlName}", options.to_json, {content_type: :json} do |response, request|
   p request
@@ -34,26 +34,34 @@ if @status < 400
 else
   puts "Crawl creation failed!"
 end
-
+=end
 
 resultStatus = 440
 resultBody = ""
 
 until resultStatus===200 do
 
-	puts "not yet: code is #{resultStatus}"
-	sleep 5
+	#puts "not yet: code is #{resultStatus}"
+	#sleep 5
 	
-    RestClient.get "https://#{userToken}:@api.80legs.com/v2/results/#{date}_#{time}_AmazonBuyBox" do |response, request|
+    #RestClient.get "https://#{userToken}:@api.80legs.com/v2/results/#{date}_#{time}_AmazonBuyBox" do |response, request|
+    RestClient.get "https://#{userToken}:@api.80legs.com/v2/results/170117_1643_AmazonBuyBox" do |response, request|
        	resultStatus = response.code
       	resultBody = response.body
     end
 end
 
-puts "the response is: #{resultBody}"
+remote_full_url = JSON.parse(resultBody)[0]
+#remote_full_url = "http://datafiniti-voltron-results.s3.amazonaws.com/x3v8wfgqhmyja687havucn1zr8yyy1fx/343093_1.txt?AWSAccessKeyId=AKIAIELL2XADVPVJZ4MA&Signature=UdbFGW96Ko53CKMpupA6fSBLuuA%3D&Expires=1485294212"
+puts remote_full_url
+remote_data = open(remote_full_url).read
 
+my_local_file = open(fileName, "w") 
+
+my_local_file.write(remote_data)
+my_local_file.close
 =begin
-RestClient.get "https://#{userToken}:@api.80legs.com/v2/results/AmazonBuyBox_170117_1438" do |response, request|
+RestClient.get "https://#{userToken}:@api.80legs.com/v2/results/170117_1643_AmazonBuyBox" do |response, request|
       p request
       p response
       p JSON.parse(response.to_s)
